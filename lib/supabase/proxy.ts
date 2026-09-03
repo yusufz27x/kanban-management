3f038a69e5ab8e61c4ebb4bb9256e907a7666ab2
Yusuf Zeybek <yusufz27x@gmail.com>
2026-09-03 22:44:06 +0300

    feat: add supabase client

---
+++ lib/supabase/proxy.ts
@@ -0,0 +1,38 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function updateSession(request: NextRequest) {
+  let response = NextResponse.next({ request });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet, headers) {
+          cookiesToSet.forEach(({ name, value }) => {
+            request.cookies.set(name, value);
+          });
+
+          response = NextResponse.next({ request });
+
+          cookiesToSet.forEach(({ name, value, options }) => {
+            response.cookies.set(name, value, options);
+          });
+
+          Object.entries(headers).forEach(([name, value]) => {
+            response.headers.set(name, value);
+          });
+        },
+      },
+    },
+  );
+
+  // Validates the access token and refreshes it when necessary.
+  await supabase.auth.getClaims();
+
+  return response;
+}
